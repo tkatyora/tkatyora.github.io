84bf470cc4bac93209f1e1c03657840ef9f86aa3
tkatyora <tkatyora7@gmail.com>
2022-07-27 23:27:47 +0200

    added the bootstrap

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+let button = document.getElementsByClassName('more-btn');
+
+ for ( x= 0; x <button.length;x++)
+ {
+    button[x].addEventListener('click',() => window.location="B:\\tnc-company-projects\\company-website\\bootstrap-website\\html-pages\\service.html");
+    
+ }
