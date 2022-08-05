d850bcea0b83c70f203ed7235155bc8d680ef702
tkatyora <tkatyora7@gmail.com>
2022-08-05 22:04:54 +0200

    admin (show password) stick nanvbar color of navbar

--- JS/admin.js
+++ JS/admin.js
@@ -1,4 +1,76 @@
-error = document.getElementsByClassName('error');
-for(x =1 ;x<error.length;x++){
-  error[x].style.display = 'none' ;
+ let error = document.getElementsByClassName('error'),
+     form = document.forms['form-id'],
+     username = document.forms["form"]['username'],
+     password = document.forms["form"]['password'],
+     show = document.querySelector('#show'),
+     input = document.getElementsByClassName('input')
+ ;
+ 
+
+for(error of error){
+  error.style.display = 'none' ;
 }
+form.addEventListener('submit',function(e){
+  e.preventDefault();
+  
+  validation();
+
+})
+
+// validation function
+  function validation() {
+   if(password.value == 2000 && username.value == 'admin'){
+    window.location = "B:\\tnc-company-projects\\company-website\\react-website\\html.pages\\admin-dashboard.html";
+   }
+   else if(password.value == 2000 && username.value != 'admin')
+   {
+    usernameWrong();
+   
+   }
+   else if(password.value != 2000 && username.value == 'admin')
+   {
+    error[1].style.display ='block';
+    passwordWrong();
+   }
+   else{
+    
+    error[0].style.display ='block';
+    bothWrong();
+   }
+   }
+show.addEventListener('click', function(){
+  if(input[1].type === 'password'){
+    input[1].type = 'text';
+    show.textContent = 'Hide'
+    
+  }else{
+    input[1].type = 'password';
+    show.textContent = 'Show'
+    
+  }
+  
+}
+
+)
+
+
+
+   // functions
+
+   function usernameWrong(){
+    error[2].style.display ='block';
+    input[0].style.border = '2px red solid';
+    input[0].style.backgroundColor = 'red'
+   }
+   function passwordWrong(){
+    error[1].style.display ='block';
+    input[1].style.border = '2px red solid';
+    
+   }
+   function bothWrong(){
+    error[2].style.display ='block';
+    input[0].style.border = '2px red solid';
+    input[1].style.border = '2px red solid';
+    
+   }
+  
